857fe019d27fe7f73379a24ac8b5ba7aad32286c
SaminoP <samopabis@gmail.com>
2026-04-13 13:16:25 +0200

    ukol 1

--- searching.py
+++ searching.py
@@ -1,30 +1,22 @@
-from pathlib import Path
 import json
 
-
 def read_data(file_name, field):
-    """
-    Reads a JSON file and returns data for a given field.
+    with open (file_name, 'r') as file:
+        data = json.load(file)
 
-    Args:
-        file_name (str): Name of the JSON file.
-        field (str): Key to retrieve from the JSON data.
-            Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
-
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
-    """
-    # get current working directory path
-    cwd_path = Path.cwd()
-    
-    file_path = cwd_path / file_name
-
+    if field in data:
+        return data[field]
+    else:
+        return None
 
 def main():
-    pass
+    sequential_data = read_data('sequential.json', 'unordered_numbers')
+    print(sequential_data)
+
+
+def linear_search(sekvence, cislo):
+
+
 
 
 if __name__ == "__main__":
